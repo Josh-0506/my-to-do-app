57ab4160188f2abaa2dd7173b8aab786eb3949d8
joshu <mail.joshuastanley@gmail.com>
2024-06-18 13:20:57 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+FilePath=r"C:\Users\joshu\OneDrive\Desktop\To Do Python\todos.txt"
+# filepath is the parameter
+def get_todos(filepath=FilePath):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+
+    return todos_local
+
+
+# this function wont return anything,no value required from this function
+def write_todos(todos_arg, filepath=FilePath):
+    with open(filepath, 'w') as file:
+      file.writelines(todos_arg)
---
+++ webpage.py
@@ -0,0 +1,26 @@
+import functions
+import streamlit as st
+import functions as ys
+
+todos = ys.get_todos()
+
+def add_todo():
+    todo=st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    ys.write_todos(todos)
+
+
+todos = ys.get_todos()
+
+st.title("My to-do App")
+st.subheader("This Is My Todo App")
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="Enter a Todo",placeholder="Add a todo..",on_change=add_todo,key='new_todo')
