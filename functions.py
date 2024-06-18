106fc4f48cd04a7202311cdc6de0d9e7118e0380
joshu <mail.joshuastanley@gmail.com>
2024-06-18 15:50:51 +0530

    Initial commit

--- functions.py
+++ functions.py
@@ -2,14 +2,14 @@ FilePath = r"C:\Users\joshu\OneDrive\Desktop\Web_app1\todos.txt"
 
 
 # filepath is the parameter
-def get_todos(filepath=FilePath):
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
+def get_todos(FilePath):
+    with open(FilePath, 'r') as file:
+        todos_local = file.readlines()
 
     return todos_local
 
 
-# this function wont return anything,no value required from this function
+# this function won't return anything,no value required from this function
 def write_todos(todos_arg, filepath=FilePath):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
